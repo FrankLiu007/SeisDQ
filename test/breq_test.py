45103ee17ee80f3db0a97ab4950900943325301c
FrankLiu007 <liuqimin2009@163.com>
2020-07-28 15:36:39 +0800

    move examples to test directory

--- test/breq_test.py
+++ test/breq_test.py
@@ -1,16 +1,20 @@
-
 from SeisDQ import DataPool
+from IRIS.breq_fast import *
 from utils import station, event
 from utils import read_json
-from reftek import fetch  
 
 if __name__ == "__main__":
 
     pars_path="par.json"
     pars=read_json.read(pars_path)
 
+    breq_path="breq.head"
+    breq_head= read_json.read(pars_path)
+
     stations=station.read(pars['station_path'])
     events=event.read_events(pars['events'])
     pool=DataPool(pars, stations, events)
     all_data=pool.process()
-    fetch(pool)
+    
+    reqs=generate_requests(pool, breq_head)
+    SendRequest(reqs)
---
+++ test/reftek_test.py
@@ -0,0 +1,19 @@
+### append sys.path first
+import sys
+sys.path.append('..')
+
+from SeisDQ import DataPool
+from utils import station, event
+from utils import read_json
+from reftek import fetch  
+
+if __name__ == "__main__":
+
+    pars_path="par.json"
+    pars=read_json.read(pars_path)
+
+    stations=station.read(pars['station_path'])
+    events=event.read_events(pars['events'])
+    pool=DataPool(pars, stations, events)
+    all_data=pool.process()
+    fetch(pool)

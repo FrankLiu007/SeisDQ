7ce4e08828da898ad2ee4ac1dcf00ed523a512af
FrankLiu007 <liuqimin2009@163.com>
2020-09-05 16:03:18 +0800

    reftek fetch modify

--- reftek/fetch.py
+++ reftek/fetch.py
@@ -1,19 +1,23 @@
-from SeisDQ import *
-from utils import event
-from utils import station
-from utils import event
-import json
+import subprocess
 import os
+import sys
+import pexpect
+
+
+from SeisDQ import *
+from utils import station, event, read_json
 #### breq_fast request to IRIS
 
 def fetch(pool):
-    requests={}
     breq=""
-    out_path=""
-    in_path=""
-    tasks=[]
+    result=[]
+    in_dir=pool.pars['input_dir']
+    out_dir=pool.pars['output_dir']
+    out_format=pool.pars['output_data_format']
+
     for eId, event in pool.events.items():
         for sId, station in pool.stations.items():
+            rr={}
             if not (sId, eId) in pool.all_data:
                 continue
             stnet=station["network"]
@@ -24,21 +28,59 @@ def fetch(pool):
             t1=pool.all_data[(sId, eId)]["time_range"][1].strftime('%Y:%j:%d:%H:%M:%S.%f')
 
             if pool.pars["data_apply_mode"]=="per_station":
-                if not os.path.exists(os.path.join(out_path,stnm)):
-                    os.mkdir(os.path.join(out_path,stnm))
-                os.system("arcfectch "+os.path.join(in_path, stnm)+ " -C *,*,*,"+t0+","+t1+ " lqm.rt")
-                os.system("rt_sac lqm.rt "+ os.path.join(out_path,stnm) )
-                
+                if not os.path.exists(os.path.join(out_dir,stnet, stnm)):
+                    x=os.mkdir(os.path.join(out_dir,stnm))
+                out_path=os.path.join(out_dir, stnet, stnm)
             elif pool.pars["data_apply_mode"]=="per_event":
-                if not os.path.exists(os.path.join(out_path, str(eId) )):
-                    os.mkdir(os.path.join(out_path, str(eId)))
-                os.system("arcfectch "+os.path.join(in_path, stnm)+ " -C *,*,*,"+t0+","+t1+ " lqm.rt")
-                os.system("rt_sac lqm.rt "+ os.path.join(out_path, str(eId) ) ) 
-    return "success"
+                if not os.path.exists(os.path.join(out_dir, str(eId) )):
+                    os.mkdir(os.path.join(out_dir, str(eId)))
+                out_path=os.path.join(out_dir, 'event'+str(eId) ) 
+
+            status, output=subprocess.getstatusoutput("arcfectch "+os.path.join(in_dir, stnm)+ " -C *,*,*,"+t0+","+t1+ " lqm.rt")
+            
+            
+            if out_format.lower()=="sac":
+                os.system("pas2sac lqm.rt "+ out_path )
+
+            elif out_format.lower()=="asc":
+                os.system("pas2asc lqm.rt "+ out_path )
+            elif out_format.lower()=="msd":
+                os.system("pas2msd lqm.rt "+ out_path )
+            elif out_format.lower()=="segy":
+                os.system("pas2segy lqm.rt "+ out_path )
+            ####
+            rr['event']=event
+            rr['time']=pool.all_data[(sId, eId)]["time_range"][0]
+            rr['path']=out_path
+            result.append(rr)
+    return result
+
+def add_sac_head(data):
+    sac=pexpect.spawn('sac')
+    for item in data:
+        sac.expect("SAC>")
+        str0="read "+ item["path"]+"/"+item["time"].strftime()+ "*"
+        sac.sendline(str0)
+
+        sac.expect("SAC>")
+        str0="ch "+ " evla "+ item['event']["latitude"] + \
+        " evlo "+ item['event']["longitude"]+ \
+            " evdp "+ item['event']["depth"]
+        sac.sendline(str0)
+
+        sac.expect("SAC>")
+        sac.sendline("wh")
+
+    sac.expect("SAC>")
+    sac.sendline("q")
+
+    return 0
+
+
 
 
 if __name__ == "__main__":
-    pars_path="par.json"
+    pars_path="../test/par.json"
     pars=read_json.read(pars_path)
 
     stations=station.read(pars['station_path'])
@@ -46,5 +88,9 @@ if __name__ == "__main__":
     pool=DataPool(pars, stations, events)
     all_data=pool.process()
 
-    fetch(pool)
-
+    in_dir=sys.argv[1]
+    out_dir=sys.argv[2]
+    out_format=sys.argv[3]
+    result=fetch(pool, in_dir, out_dir, out_format)
+##examples to add headers to file 
+    add_sac_head(result)   
--- test/reftek_test.py
+++ test/reftek_test.py
@@ -1,19 +1,28 @@
 ### append sys.path first
 import sys
+import argparse
 sys.path.append('..')
 
 from SeisDQ import DataPool
 from utils import station, event
 from utils import read_json
-from reftek import fetch  
+from reftek import fetch, add_sac_head
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--par", help="location of the parameters file .")
+    args=parser.parse_args()
 
-    pars_path="par.json"
-    pars=read_json.read(pars_path)
+    # pars_path="par.json"
+    pars=read_json.read(args.par)
 
     stations=station.read(pars['station_path'])
     events=event.read_events(pars['events'])
     pool=DataPool(pars, stations, events)
     all_data=pool.process()
-    fetch(pool)
+
+
+    result=fetch(pool)
+##examples to add headers to file 
+    add_sac_head(result)   
